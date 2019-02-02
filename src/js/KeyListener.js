d4aa6e0d5984a3c7f0cc4d138eead9a98c51faf1
gunesyurdakul <gunesyurdakul@gmail.com>
2019-02-03 00:00:07 +0100

    fix up key

--- src/js/KeyListener.js
+++ src/js/KeyListener.js
@@ -3,15 +3,17 @@
 function startKeyListener(){
   document.addEventListener("keydown", function(event) {
     //clicked right-arrow or down-arrow, and ignore edge case (last subchapter)
-    if ((event.which == '39' || event.which == '40')&&(data_loader.subchapters.indexOf(data_loader.active_subchapter)+1 <= Object.keys(data_loader.subchapters).length)){
+    if ((event.which == '39' || event.which == '40')&&(data_loader.active_subchapter.index+1<Object.keys(data_loader.subchapters).length)){
       next_subchapter_id= Object.keys(data_loader.subchapters)[Object.keys(data_loader.subchapters).indexOf(data_loader.active_subchapter.id)+1]
       subchapterClick(next_subchapter_id.split('-')[0],next_subchapter_id.split('-')[1]);
-
+      console.log("down",event.which);
+      
     }
     //clicked left-arrow or up-arrow, and ignore edge case (first subchapter)
-    else if((event.which == '37' || event.which == '38')&&(data_loader.subchapters.indexOf(data_loader.active_subchapter)-1 >0)){
+    else if((event.which == '37' || event.which == '38')&&(data_loader.active_subchapter.index>0)){
       previous_subchapter_id = Object.keys(data_loader.subchapters)[Object.keys(data_loader.subchapters).indexOf(data_loader.active_subchapter.id)-1]
       subchapterClick(previous_subchapter_id.split('-')[0],previous_subchapter_id.split('-')[1]);
+      console.log("up",event.which);
     }
   });
 }
--- src/js/Subchapter.js
+++ src/js/Subchapter.js
@@ -1,13 +1,13 @@
 class Subchapter{
 
-    constructor(subchapter, chapter){
+    constructor(index,subchapter, chapter){
         this.id  = subchapter["number"].replace(".","-");
         this.title = subchapter["name"];
         this.summary = subchapter['summary'];
         this.loc_view = subchapter['location_view'];
         this.chapter = chapter;
         this.has_image = subchapter["image"];
-
+        this.index=index;
         this.has_static_fig = false;
         this.static_fig_title = "none";
         this.figures = []
