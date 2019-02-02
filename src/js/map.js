1e89ffa9c0fa5bfa764e8d216f7039e213431a27
gunesyurdakul <gunesyurdakul@gmail.com>
2019-02-02 16:44:42 +0100

    add home and tutorial buttons, change base layer

--- src/js/map.js
+++ src/js/map.js
@@ -31,7 +31,7 @@
 });
 
 // define tile layer
-var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
     subdomains: 'abcd',
     maxZoom: 19,
